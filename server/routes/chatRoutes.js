56cd4eb4560883da3db5cd3747d5105a8a002d88
Jainil Shah <jainilcrshah@gmail.com>
2026-04-28 21:03:21 +0530

    group chat test

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { protect } from '../middlewares/authMiddleware.js'
 import { accessChat } from '../controllers/chatController.js';
+import { fetchChats } from '../controllers/chatController.js';
+import { createGroupChat } from '../controllers/chatController.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, accessChat)
-// router.route('/').get(protect, fetchChats)
-// router.route('/').post(protect, accessChat)
-// router.route("/group").post(protect, createGroupChat)
+router.route('/').get(protect, fetchChats)
+router.route("/group").post(protect, createGroupChat)
 // router.route("/rename").put(protect, renameGroupChat)
 // router.route("/groupremove").put(protect, removeUserFromGroup)
 // router.route("/groupadd").put(protect, addUsersToGroup)
