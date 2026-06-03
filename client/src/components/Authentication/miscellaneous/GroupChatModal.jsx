e9e5159acb6329e03d720c308abf7e2dd7f8ee6c
Jainam Shah <jainamshah1527@gmail.com>
2026-06-03 10:54:47 +0530

    groupchatmodal

--- client/src/components/Authentication/miscellaneous/GroupChatModal.jsx
+++ client/src/components/Authentication/miscellaneous/GroupChatModal.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Button, Modal, ModalBody, ModalCloseButton, ModalHeader, ModalOverlay, useDisclosure ,  ModalFooter, ModalContent, Text } from "@chakra-ui/react";
-
+import React, { use, useState } from "react";
+import {  Button, Modal, ModalBody, ModalCloseButton, ModalHeader, ModalOverlay, useDisclosure ,  ModalFooter, ModalContent, Text, useToast, FormControl, Input } from "@chakra-ui/react";
+import { ChatState } from "../../../Context/ChatProvider";
 const GroupChatModal = ({children}) => {
 
     const { isOpen, onOpen, onClose } = useDisclosure();
-    
+    const [groupChatName, setGroupChatName] = useState()
+    const [selectedUsers, setselectedUsers] = useState([])
+    const [search, setSearch] = useState("")
+    const[searchResult, setSearchResult]= useState([])
+    const [loading, setLoading] = useState(false);
+
+
+    const toast = useToast()
+  
+     const { user, chats , setChats} = ChatState(); 
+    const handleSearch = () => {}
+
     return ( 
      <>
        <Button onClick={onOpen}>
@@ -12,11 +23,24 @@ const GroupChatModal = ({children}) => {
        </Button>
        <Modal isOpen={isOpen} onClose={onClose}>
            <ModalOverlay/>
-           <ModalContent>
-           <ModalHeader>Modal Title</ModalHeader>
+           <ModalContent  >
+           <ModalHeader fontSize="35px"
+           fontFamily="Work sans"
+           display="flex"
+           justifyContent="center">Create Group Chat</ModalHeader>
            <ModalCloseButton/>
-           <ModalBody>
-             <Text>Sample text is here</Text>
+           <ModalBody diplay="flex" flexDir="column" alignItems="center">
+              <FormControl>
+                 <Input placeholder=" Chat Name" mb={3} onChange={(e) => setGroupChatName(e.target.value)} />
+              </FormControl>
+              <FormControl>
+                <Input placeholder=" Add Users eg: Jainam, Aryan, Teerth" 
+                mb={1} 
+                onChange={(e) => handleSearch(e.target.value)} />
+              </FormControl>
+               
+                {/* render searched users here  */}
+
            </ModalBody>
 
            <ModalFooter>
