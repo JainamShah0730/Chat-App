136bce08cfecc3118af8fb9ad85d30b2a315ad90
Jainam Shah <jainamshah1527@gmail.com>
2026-06-25 02:51:34 +0530

    changes

--- client/src/components/SingleChat.jsx
+++ client/src/components/SingleChat.jsx
@@ -13,7 +13,7 @@ import io from "socket.io-client"
 import Lottie from "lottie-react"
 import animationData from "../Animation/Typing.json"
 
-const ENDPOINT = "http://localhost:5000"
+const ENDPOINT = import.meta.env.VITE_API_URL;
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
