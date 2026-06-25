94715f13a6eb5a601c5cb20da49f283bc748f925
Jainam Shah <jainamshah1527@gmail.com>
2026-06-25 11:06:10 +0530

    fixing typing

--- client/src/components/SingleChat.jsx
+++ client/src/components/SingleChat.jsx
@@ -214,12 +214,12 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                </div>
                ) }
                <FormControl onKeyDown={sendMessage} isRequired mt={3}>
-                {isTyping ? ( 
+                {isTyping && ( 
                   <Box bg="#E8E8E8" borderRadius="20px" p="5px 15px" width="fit-content" mb={3} ml={1}>
                      <Spinner size="xs" mr={2} />
                      <Text as="span" fontSize="sm" color="gray.500">Typing...</Text>
                   </Box>
-                ) :( <></> )}
+                )}
                 <Input 
                 variant="filled"
                 bg="#E0E0E0"
